5c3460492d361041ab3cb64d79d31b0c61b0f8ba
Chuy Del Castillo <chuy@imchuy.com>
2015-07-13 10:23:01 -0700

    Updated

---
+++ osx-host/.xmonad/scripts/statusbar.sh
@@ -0,0 +1,47 @@
+# ICONS HOME
+ICONS_HOME="/home/chuy/.xmonad/icons"
+
+function Battery() {
+    # BATTERY
+    local BATTERY=$(acpi | sed 's/[,%]//g' | awk '{print $3 " " $4}')
+    local BATTERY_STATUS=$(echo $BATTERY | cut -f 1 -d " ")
+    local BATTERY_PERCENTAGE=$(echo $BATTERY | cut -f 2 -d " ")
+    if [ $BATTERY_PERCENTAGE -gt 70 ]; then
+        local COLOR="^fg(green)"
+        local ICON="^i($ICONS_HOME/bat_full_01.xbm)"
+    elif [ $BATTERY_PERCENTAGE -gt 35 ]; then
+        local COLOR="^fg(orange)"
+        local ICON="^i($ICONS_HOME/bat_low_01.xbm)"
+    else
+        local COLOR="^fg(red)"
+        local ICON="^i($ICONS_HOME/bat_empty_01.xbm)"
+    fi
+
+    if [ $BATTERY_STATUS == "Charging" ]; then
+        local COLOR="^fg(green)"
+    fi
+
+    echo "$COLOR $ICON ^fg()$BATTERY_PERCENTAGE%"
+}
+
+function Updates(){
+    local ICON="^fg(yellow)^i($ICONS_HOME/pacman.xbm)"
+    echo "$ICON  ^fg()$(pacman -Qu | wc -l)"
+}
+
+function Date() {
+    echo $(date +"%Y-%m-%d - %R")
+}
+
+# MAIN FUNCTION
+function Status() {
+    BATTERY=$(Battery)
+    DATE=$(Date)
+    UPDATES=$(Updates)
+
+    echo "$UPDATES $BATTERY $DATE"
+}
+
+# LOOP
+
+while true; do echo $(Status) > /tmp/xmonad.status; sleep 1; done;
