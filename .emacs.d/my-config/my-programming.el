fb8ae742e56497adc2a70199f64309d972ecfdaf
Chuy Martinez <chuy@mixmax.com>
2018-03-14 10:36:02 -0700

    Add javascript flow support.

--- .emacs.d/my-config/my-programming.el
+++ .emacs.d/my-config/my-programming.el
@@ -10,12 +10,17 @@
 
 ;; Enable flycheck globally
 (require 'flycheck)
+(require 'flycheck-flow)
 (global-flycheck-mode)
 
 ;; Company mode
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
-
+(setq company-minimum-prefix-length 1)
+(with-eval-after-load 'company
+  (lambda ()
+    (add-to-list 'company-backends 'company-tern)
+    (add-to-list 'company-backends 'company-flow)))
 
 ;; Display flycheck errors at the bottom on a small-ish window
 (add-to-list 'display-buffer-alist
@@ -29,6 +34,15 @@
 ;; Javascript configs
 (require 'js2-mode)
 (setq js2-basic-offset 2)
+(setq js2-mode-show-parse-errors nil)
+(setq js2-mode-show-strict-warnings nil)
+
+;; flow
+
+(with-eval-after-load 'flycheck
+  (flycheck-add-mode 'javascript-flow 'flow-minor-mode)
+  (flycheck-add-mode 'javascript-eslint 'flow-minor-mode)
+  (flycheck-add-next-checker 'javascript-flow 'javascript-eslint))
 
 (defun js2-highlight-undeclared-vars ()
   "Override js2-mode undeclared vars feature, I use eslint and jshint anyway."
@@ -46,8 +60,15 @@
 (add-hook 'js2-mode-hook
           '(lambda()
              (tern-mode t)
-             (unless (member 'company-tern 'company-backends)
-               (add-to-list 'company-backends 'company-tern))))
+             (flow-minor-mode)))
+
+(require 'rjsx-mode)
+
+(with-eval-after-load 'rjsx-mode
+  (define-key rjsx-mode-map "<" nil)
+  (define-key rjsx-mode-map (kbd "C-d") nil)
+  (define-key rjsx-mode-map ">" nil))
+
 (provide 'my-programming)
 
 ;;; my-programming.el ends here
